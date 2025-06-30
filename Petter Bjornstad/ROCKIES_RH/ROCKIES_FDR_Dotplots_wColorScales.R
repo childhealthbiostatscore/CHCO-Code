596eeb3dbe6cd358e192475d45871c05b8ad7cd4
Dylan <135753623+netiony@users.noreply.github.com>
2025-06-30 11:03:32 -0400

    Update ROCKIES_FDR_Dotplots_wColorScales.R

--- Petter Bjornstad/ROCKIES_RH/ROCKIES_FDR_Dotplots_wColorScales.R
+++ Petter Bjornstad/ROCKIES_RH/ROCKIES_FDR_Dotplots_wColorScales.R
@@ -51,14 +51,18 @@ library(doParallel)
 
 load('C:/Users/netio/Documents/UofW/Rockies/Line4875_Rockies.RData')
 
+#For Both 
+
 
 
 #Function
-kidneyimaging_analysis <- function(celltype, genes, gene_list_name = 'TCA', 
+kidneyimaging_analysis <- function(celltype, genes, gene_list_name = 'TCA', T2D_Only = TRUE, 
                                    dir.results, cl_number = 1, cpc = 0.005, 
                                    set_cutoff = F, logFC_thresh = 10, pvalue_thresh = 0.1){
   
-  if(celltype == 'PT'){
+  if(celltype == 'All'){
+    so_celltype <- sO_subset
+  }else if(celltype == 'PT'){
     so_celltype <- subset(so_subset,celltype2 == celltype)
     cat('PT Cells')
   }else if(celltype == 'TAL'){
@@ -184,121 +188,106 @@ kidneyimaging_analysis <- function(celltype, genes, gene_list_name = 'TCA',
     
     #Make dataframe of final results
     full_results <- as.data.frame(nebula_summaries)
-    
-    if(nrow(full_results) == 0){
-      next
-    }
-    
-    if(is.null(adjustment)){
-      colnames(full_results) <- c("logFC_Intercept","logFC","se_Intercept","se","p_Intercept",
-                                  "p_value","gene_id","gene","Gene")
-    }else{
-      colnames(full_results) <- c("logFC_Intercept","logFC",'logFC_SGLTi2', "se_Intercept",
-                                  "se",'se_SGLTi2', "p_Intercept","p_value",'p_value_SGLTi2', "gene_id","gene",
-                                  "Gene")
-    }
-    
-    
-    
-    full_results$Variable <- exposure
     #Calculate number of genes filtered out for low expression 
-    full_results$low_exp <- length(tca_genes)-length(full_results$gene)
+    low_exp <- length(tca_genes)-length(full_results$gene)
     #Filter out non-converging genes
     full_results <- full_results %>% 
       filter(!gene %in%  nonconverge_genes)
     #Calculate nonconvergence rate
-    full_results$nebula_nonconverged_percent <- paste0(round((1-(length(tca_genes)-length(nonconverge_genes))/length(tca_genes))*100,3),"%")
+    nebula_nonconverged_percent <- paste0(round((1-(length(tca_genes)-length(nonconverge_genes))/length(tca_genes))*100,3),"%")
     # nebula_nonconverged_percent <- (length(rownames(counts_path))-length(unique(full_results$gene)))/length(rownames(counts_path))
     # print(paste0(nebula_nonconverged_percent*100, "% failed to converge"))
-    full_results$fdr <- p.adjust(full_results$p_value,method="fdr")  
+    full_results <- full_results %>%
+      mutate(fdr=p.adjust(`p_epic_sglti2_1Yes`,method="fdr"))  
     # mutate(fdr3=p.adjust(PValue3,method="fdr"))
-    full_results$PValue10 <- -log10(pmax(full_results[,6], 1e-10))
+    full_results$PValue10 <- -log10(pmax(full_results$`p_epic_sglti2_1Yes`, 1e-10))  # Avoid log(0)
     
-    
-    
+    write.csv(full_results,fs::path(dir.results,paste0("NEBULA_TCA_cycle_",celltype2,"_cells_SGLT2_T2D_Only_unadjusted_pooled_offset.csv")))
+
     total_results <- rbind(total_results,full_results)
   }
-  
-  if(is.null(adjustment)){
+
     file.name <- paste0(dir.results, 'NEBULA_', gene_list_name, 
-                        '_median', median, '_', celltype, '_cells_PET_unadjusted_pooled_offset_T2D.txt')
-  }else{
-    file.name <- paste0(dir.results, 'NEBULA_', gene_list_name, 
-                        '_median', median, '_', celltype, '_cells_PET_adjusted_pooled_offset_T2D.txt')
-  }
+                        '_', celltype, '_cells_PET_adjusted_pooled_offset_T2D_SGLT2i.txt')
+
   
   
   write.table(total_results,file.name, row.names=F, quote=F, sep='\t')
   
-  cat(paste0(nrow(full_results), ' genes passed QC and were analysed in NEBULA \n'))
   # total_results <- read.csv(fs::path(dir.results,"NEBULA_TCA_cycle_PT_cells_PET_Variables_unadjusted_pooled_offset.csv"))
   
   # Define significance stars
-  total_results <- total_results %>%
-    mutate(signif = case_when(
-      fdr < 0.01 ~ "**",
-      fdr < 0.05 ~ "*",
-      TRUE ~ ""
-    ))
+  
+  full_results$color <- ifelse(full_results$fdr < 0.05 & full_results$`logFC_epic_sglti2_1Yes` > 0, "lightcoral",
+                               ifelse(full_results$fdr < 0.05 & full_results$`logFC_epic_sglti2_1Yes` < 0, "lightblue", "gray"))
+  
+  # Identify significant points (fdr < 0.05)
+  significant_df <- full_results[full_results$fdr < 0.05, ]
+  
+  Genes <- length(unique(full_results$gene))
+  Cell <- ncol(so_celltype)
+  Nonconvergence_Rate <- nebula_nonconverged_percent
+  # full_results$color3 <- ifelse(full_results$fdr3 < 0.2 & full_results$`logFC_epic_sglti2_1Yes`3 > 0, "lightcoral",
+  #                               ifelse(full_results$fdr3 < 0.2 & full_results$`logFC_epic_sglti2_1Yes`3 < 0, "lightblue", "gray"))
+  # 
+  # # Identify significant points (fdr < 0.05)
+  # significant_df3 <- full_results[full_results$fdr3 < 0.2, ]
+  
+  max <- max(full_results$`logFC_epic_sglti2_1Yes`)
+  # max <- 3.1
+  min <- min(full_results$`logFC_epic_sglti2_1Yes`)
+  
+  dot_plot_tca <- ggplot(full_results, aes(
+    y = gene,
+    x = `logFC_epic_sglti2_1Yes`,
+    color = fdr,
+    size = abs(`logFC_epic_sglti2_1Yes`)
+  )) +
+    geom_point(alpha = 0.7) +
+    binned_scale(aesthetics = 'color', scale_name = 'stepsn', 
+                 palette = function(x) c('red', 'orange', 'purple', 'blue', 'grey'),
+                 breaks = c(0.05, 0.1, 0.15, 0.2, 1),
+                 limits = c(0, 1), 
+                 show.limits = TRUE, guide = 'colorsteps') +
+    scale_size(range = c(2, 6), name = "|LogFC|") +
+    geom_vline(xintercept = 0, linetype = "dashed", color = "black") +
+    theme_minimal() +  # Retains grid lines
+    labs(
+      title = paste0("Differentially Expressed TCA Cycle Genes in ",celltype," Cells"),
+      subtitle = "SGLT2i vs. No SGLT2i (T2D Only), Unadjusted (Pooled Offset)",
+      x = "Log Fold Change",
+      y = "Gene",
+      caption = paste0(
+        "FDR < 0.05, Genes = ", Genes,
+        ", Cells = ", Cell,
+        ", Non-Convergence Rate: ", Nonconvergence_Rate,
+        ", Genes Filtered out for Low Expression: ", low_exp
+      )
+    ) +
+    theme(
+      plot.title = element_text(hjust = 0),
+      axis.text.y = element_text(size = 8),
+      # axis.text.x = element_text(angle = 0, hjust = 1),
+      axis.line = element_line(color = "black", size = 0.5),
+      axis.ticks.x = element_line(color = "black"),
+      panel.border = element_blank(),
+      panel.background = element_blank()
+    )
+  
+  
+  
   
   # Select only the needed columns and rename LogFC for clarity
-  if(median == T){
-    subtitle1 <- paste0(celltype, ' (Median), ')
-  }else{
     subtitle1 <- paste0(celltype, ', ')
-  }
-  
-  if(!is.null(adjustment)){
-    subtitle2 <- paste0('with adjustment for ', adjustment)
-  }else{
     subtitle2 <- paste0('with no adjustment')
-  }
   subtitle <- paste0(subtitle1, subtitle2)
   
   
-  if(set_cutoff == TRUE){
-    rem_index <- which(abs(total_results$logFC) > logFC_thresh & total_results$p_value > pvalue_thresh)
-    total_results$logFC[rem_index] <- NA
-  }
-  
-  
-  heatmap_data <- total_results %>%
-    dplyr::select(Gene, Variable, logFC, signif)
 
-  
-  
-  heat_map_p <- ggplot(heatmap_data, aes(x = Variable, y = Gene, fill = logFC)) +
-    geom_tile(color = "grey90") +
-    geom_text(aes(label = signif), size = 3, color = "black") +
-    scale_fill_gradient2(
-      low = "#264653", mid = "white", high = "darkred",
-      midpoint = 0,
-      name = "LogFC"
-    ) +
-    theme_minimal() +
-    labs(title = paste0(gene_list_name, " Genes vs. PET Variables (T2D)"),
-         subtitle = subtitle,
-         x = "Exposure",
-         y = "Gene") +
-    scale_x_discrete(labels = setNames(custom_labels, custom_order))+
-    theme(
-      text = element_text(face="bold"),
-      axis.text.y = element_text(size = 6),
-      axis.text.x = element_text(angle = 45, hjust = 1),
-      axis.ticks.y = element_blank(),
-      axis.title.x = element_blank(),
-      panel.grid = element_blank()
-    )
-  
   # custom_colors <- c("#264653", "#2a9d8f", "#e9c46a", "#f4a261", "#e76f51", "darkred")
-  
-  if(is.null(adjustment)){
     file.name <- paste0(dir.results, 'NEBULA_', gene_list_name, 
-                        'NEBULA_','median', median, '_', celltype, '_PET_unadjusted_pooled_offset_T2D.png')
-  }else{
-    file.name <- paste0(dir.results, 'NEBULA_', gene_list_name, 
-                        'NEBULA_', 'median', median, '_', celltype, '_PET_adjusted_pooled_offset_T2D.png')  
-  }
+                        '_', celltype, '_pooled_offset_T2D_SGLT2i.png')  
+
   
   
   # Ensure clean graphics device state
