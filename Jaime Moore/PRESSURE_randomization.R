3689ab680dc2388c66396b61391164263f486cbb
laurapyle <laurapyle@gmail.com>
2025-07-18 10:50:03 -0700

    coding pt source

--- Jaime Moore/PRESSURE_randomization.R
+++ Jaime Moore/PRESSURE_randomization.R
@@ -10,7 +10,7 @@ obs <- as.data.frame(obs)
 obs <- blockrand(n=nrow(obs), levels = c(0:1), block.sizes = 1)
 obs$treatment_char <- NA
 obs$treatment_char <- ifelse(obs$treatment==1, "SEMAGLUTIDE", "USUAL_CARE")
-obs$intinclusion7 <- "OBS"
+obs$source <- "OBS"
 
 # existing patient pool
 existing <- rep("EXISTING", 100)
@@ -18,14 +18,16 @@ existing <- as.data.frame(existing)
 existing <- blockrand(n=nrow(existing), levels = c(0:1), block.sizes = 1)
 existing$treatment_char <- NA
 existing$treatment_char <- ifelse(existing$treatment==1, "SEMAGLUTIDE", "USUAL_CARE")
-existing$intinclusion7 <- "EXISTING"
+existing$source <- "EXISTING"
 
 # combine
 rand <- rbind(obs, existing)
+rand$intinclusion7 <- ifelse(rand$source == "OBS", 0, 1)
 
 # check
 table(rand$treatment_char)
-table(rand$intinclusion7, rand$treatment_char)
+table(rand$source, rand$treatment_char)
+table(rand$source, rand$intinclusion7)
 
 # output
 write.csv(rand, 
