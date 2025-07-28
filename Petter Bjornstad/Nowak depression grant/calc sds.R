31a2900ca22866be17742d57670c05bf1396b115
laurapyle <laurapyle@gmail.com>
2025-07-27 17:46:40 -0700

    Create calc sds.R

---
+++ Petter Bjornstad/Nowak depression grant/calc sds.R
@@ -0,0 +1,7 @@
+
+
+data <- read.csv('/Users/laurapyle/Library/CloudStorage/OneDrive-UW/Nowak/GLP1-Ra depression grant/Power calculations/PENGUIN-BrainBiomarkers_DATA_2025-07-27_1815.csv')
+
+sd(data$nfl_avg_conc)
+sd(data$gfap_avg_conc)
+sd(data$ptau_181_avg_conc)
