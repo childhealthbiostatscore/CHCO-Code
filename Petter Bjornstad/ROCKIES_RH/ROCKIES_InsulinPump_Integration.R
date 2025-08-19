62520133fac0decdb7605e6a09a6f72d91d1aec1
Dylan <135753623+netiony@users.noreply.github.com>
2025-08-19 09:55:59 -0700

    Update ROCKIES_InsulinPump_Integration.R

--- Petter Bjornstad/ROCKIES_RH/ROCKIES_InsulinPump_Integration.R
+++ Petter Bjornstad/ROCKIES_RH/ROCKIES_InsulinPump_Integration.R
@@ -50,10 +50,57 @@ library(doParallel)
 
 
 
+insulin_genes <- c("INSR", "IGF1R", "INSRR",
+                     "IRS1", "IRS2", "IRS4", "GRB2", "SHC1",
+                     "PIK3CA", "PIK3CB", "PIK3CD", "PIK3R1", "PIK3R2",
+  "AKT1", "AKT2", "AKT3",
+  "PDPK1", "PTEN", "INPP4B",
+  "MTOR", "RPTOR", "RICTOR", "MLST8",
+  "AKT1S1", "TSC1", "TSC2", "RHEB",
+  "EIF4EBP1", "RPS6KB1", "RPS6", "EIF4E",
+  "SLC2A1", "SLC2A2", "SLC2A4",
+  "SLC5A1", "SLC5A2",
+  "HK1", "HK2", "GCK",
+  "PFKFB2", "PFKFB3", "PKM", "LDHA",
+  "PCK1", "PCK2", "G6PC", "FBP1", "FBP2",
+  "FASN", "ACACA", "ACACB", "SCD",
+  "SREBF1", "SREBP2", "INSIG1", "INSIG2",
+  "CPT1A", "CPT2", "ACOX1",
+  "PPARGC1A", "PPARGC1B", "NRF1", "TFAM",
+  "ATP5F1A", "COX4I1", "NDUFS1", "SDHA",
+  "ATP5F1A", "COX4I1", "NDUFS1", "SDHA",
+  "PTPN1", "PTPRF", "PTPN11",
+  "SOCS1", "SOCS3", "SOCS7",
+  "PRKAA1", "PRKAA2",
+  "FOXO1", "FOXO3", "FOXO4")
 
 
 
 
+
+
+
+
+
+
+
+#PT 
+
+
+
+
+
+
+
+
+#TAL 
+
+
+
+
+
+
+#EC 
 
 
 
