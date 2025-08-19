02e2463a7f5c5b67a21d29c6f4eff38b5324d371
Dylan <135753623+netiony@users.noreply.github.com>
2025-08-19 10:56:42 -0700

    Update ROCKIES_YeJiBoxPlots.R

--- Petter Bjornstad/ROCKIES_RH/ROCKIES_YeJiBoxPlots.R
+++ Petter Bjornstad/ROCKIES_RH/ROCKIES_YeJiBoxPlots.R
@@ -46,6 +46,66 @@ library(nebula)
 
 load('C:/Users/netio/Documents/UofW/Rockies/Line438_Boxplots_NoMed.RData')
 
+harmonized_data <- read.csv("C:/Users/netio/Documents/Harmonized_data/harmonized_dataset.csv", na = '')
+
+
+dat <- harmonized_data %>% dplyr::select(-dob) %>% 
+  arrange(screen_date) %>% 
+  dplyr::summarise(across(where(negate(is.numeric)), ~ ifelse(all(is.na(.x)), NA_character_, first(na.omit(.x)))),
+                   across(where(is.numeric), ~ ifelse(all(is.na(.x)), NA_real_, first(na.omit(.x)))),
+                   .by = c(record_id, visit))
+
+
+PET_avg <- function(data){
+  tmp_df <- data %>% dplyr::select(lc_k2, rc_k2, lm_k2, rm_k2,
+                                   lc_f, rc_f, lm_f, rm_f)
+  avg_c_k2 <- tmp_df %>%
+    dplyr::select(lc_k2, rc_k2) %>% rowMeans(na.rm=T)
+  
+  avg_m_k2 <- tmp_df %>% 
+    dplyr::select(lm_k2, rm_k2) %>% rowMeans(na.rm=T)
+  
+  avg_c_f <- tmp_df %>% 
+    dplyr::select(lc_f, rc_f) %>% rowMeans(na.rm=T)
+  
+  avg_m_f <- tmp_df %>% 
+    dplyr::select(lm_f, rm_f) %>% rowMeans(na.rm=T)
+  
+  avg_c_k2_f <- avg_c_k2 / avg_c_f
+  
+  avg_m_k2_f <- avg_m_k2 / avg_m_f
+  
+  results <- bind_cols(avg_c_k2, avg_m_k2, avg_c_f, avg_m_f, 
+                       avg_c_k2_f, avg_m_k2_f) %>% as.data.frame()
+  names(results) <- c('avg_c_k2', 'avg_m_k2', 'avg_c_f', 'avg_m_f', 
+                      'avg_c_k2_f', 'avg_m_k2_f')
+  
+  return(results)
+  
+}
+
+
+tmp_results <- PET_avg(dat)
+
+
+dat_results <- dat %>% dplyr::select(-avg_c_k2, -avg_m_k2, -avg_c_f)
+
+dat_results <- dat_results %>% bind_cols(tmp_results)
+
+dat_results <- dat_results %>% filter(!is.na(avg_c_k2))
+
+table1::table1(~age + sex + bmi + study + group + epic_sglti2_1 + 
+                 avg_c_k2 + avg_c_f + avg_m_k2 + avg_m_f + avg_c_k2_f + avg_m_k2_f | group, data = dat_results)
+
+
+
+
+
+
+
+
+
+
 tests <- c('avg_c_k2', 'avg_c_f', 
            'avg_m_k2', 'avg_m_f', 
            'avg_c_k2_f', 'avg_m_k2_f')
