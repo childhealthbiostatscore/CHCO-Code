4e874b1a7a1848abac43d4a9c54fb09eca55ba2c
Dylan <135753623+netiony@users.noreply.github.com>
2025-08-22 12:00:47 -0700

    Create ROCKIES_Dexa_NEBULA.R

---
+++ Petter Bjornstad/ROCKIES_RH/ROCKIES_Dexa_NEBULA.R
@@ -0,0 +1,14 @@
+#DEXA Analysis
+
+
+
+
+
+
+
+
+
+
+dex_var <- c("dexa_ag_ratio",  "dexa_body_fat",            "dexa_bone_mineral_density", "dexa_est_vat",             
+             "dexa_fat_kg",               "dexa_lean_kg",              "dexa_lean_mass",            "dexa_trunk_kg",            
+            "dexa_trunk_mass"  )
