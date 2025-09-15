375e5d326d897b3fd91aadbf27f3554d7d6b520e
Dylan <135753623+netiony@users.noreply.github.com>
2025-09-15 11:37:32 -0700

    Create MMP9_DKA_Study_Updates.R

---
+++ Petter Bjornstad/MMP9/MMP9_DKA_Study_Updates.R
@@ -0,0 +1,51 @@
+#### DKA MMP9 Study 
+
+library(dplyr)
+library(stringr)
+library(ggplot2)
+
+
+
+raw_data <- data.table::fread('C:/Users/netio/Documents/UofW/Projects/MMP9/Full 16-1403 DKA RedCap Raw Data Set_04-24edited.csv')
+
+
+names_rawdata <- names(raw_data)
+mmp9_index <- str_which(names_rawdata, 'mmp9')
+
+data_dictionary <- readxl::read_xlsx('C:/Users/netio/Documents/UofW/Projects/MMP9/Full 16-1403 DKA RedCap Data Key_04-24edited.xlsx')
+
+
+final_data <- data.table::fread('C:/Users/netio/Documents/UofW/Projects/MMP9/Full_DKA_Data Set_06-26_final.csv')
+kidney_data <- data.table::fread('C:/Users/netio/Documents/UofW/Projects/MMP9/Kidney_DATA_2020-12-03_1614.csv')
+
+qcr <- data.table::fread('C:/Users/netio/Documents/UofW/Projects/MMP9/qcr.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
