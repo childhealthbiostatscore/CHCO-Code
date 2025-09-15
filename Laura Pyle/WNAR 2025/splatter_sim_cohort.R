614141729e0c8a7c88b9a350211c5c61c344e33b
laurapyle <laurapyle@gmail.com>
2025-09-15 07:18:12 -0700

    add more simulation parameters

--- Laura Pyle/WNAR 2025/splatter_sim_cohort.R
+++ Laura Pyle/WNAR 2025/splatter_sim_cohort.R
@@ -9,13 +9,18 @@ simulate_cohort_specific <- function(n_controls = 5,
                                      n_patients = 8,
                                      control_cells_mean = 180,
                                      patient_cells_mean = 120,
-                                     correlation = 0.4) {
+                                     control_cells_var = 30,
+                                     patient_cells_var = 30,
+                                     correlation = 0.4,
+                                     n_genes = 3000,
+                                     batch_facLoc = 0.1,
+                                     batch_facScale = 0.25) {
   
   # Controls often have better cell yield
-  control_counts <- round(rnorm(n_controls, control_cells_mean, 30))
+  control_counts <- round(rnorm(n_controls, control_cells_mean, control_cells_var))
   
   # Patients might have more variable/lower yields
-  patient_counts <- round(rnorm(n_patients, patient_cells_mean, 50))
+  patient_counts <- round(rnorm(n_patients, patient_cells_mean, patient_cells_var))
   
   # Combine
   all_counts <- c(control_counts, patient_counts)
@@ -24,7 +29,8 @@ simulate_cohort_specific <- function(n_controls = 5,
   sim <- simulate_with_correlation(
     n_individuals = n_controls + n_patients,
     cells_per_ind = all_counts,
-    within_ind_correlation = correlation
+    within_ind_correlation = correlation,
+    n_genes = n_genes
   )
   
   # Add disease status
@@ -41,19 +47,19 @@ simulate_cohort_specific <- function(n_controls = 5,
 
 sim_cohort <- simulate_cohort_specific(
   n_controls = 4,
-  n_patients = 6
-)
+  n_patients = 6,
+  control_cells_mean = 180,
+  patient_cells_mean = 120,
+  control_cells_var = 30,
+  patient_cells_var = 30,
+  correlation = 0.4,
+  n_genes = 1000,
+  batch_facLoc = 0.1,
+  batch_facScale = 0.25)
 
 # Check distribution by disease status
 table(colData(sim_cohort)$DiseaseStatus, colData(sim_cohort)$Individual)
-control_cells <-  colData(sim_cohort)$DiseaseStatus == "Control"
-tx_cells <- colData(sim_cohort)$DiseaseStatus == "Treatment"
-sce_control <- sim_cohort[, control_cells]
-sce_tx <- sim_cohort[, tx_cells]
-sce_list <- list()
-sce_list[["Control"]] <- sce_control
-sce_list[["CKM"]] <- tx_cells
 
-comparison <- compareSCEs(sce_list)
-names(comparison$Plots)
-comparison$Plots$Means
+# remaining workflow
+# visualize simulation
+# simulate remaining data and save
