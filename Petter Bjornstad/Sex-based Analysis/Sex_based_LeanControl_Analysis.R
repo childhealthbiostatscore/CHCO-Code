622eb6ef26850fcdcd763e047fd1618cf6b1b5c5
Dylan <135753623+netiony@users.noreply.github.com>
2025-10-13 14:02:23 -0700

    Update Sex_based_LeanControl_Analysis.R

--- Petter Bjornstad/Sex-based Analysis/Sex_based_LeanControl_Analysis.R
+++ Petter Bjornstad/Sex-based Analysis/Sex_based_LeanControl_Analysis.R
@@ -834,10 +834,86 @@ folder_path <- "/Users/netio/Documents/UofW/Projects/Sex_based_Analysis/LeanCont
 
 
 
+ 
+ #### Proteomics and Metabolomics 
+ 
+ 
+ 
+ 
+ library(scran)
+ library(future)
+ library(future.apply)
+ library(tidyverse)
+ library(colorspace)
+ library(patchwork)
+ library(ggdendro)
+ library(cowplot)
+ library(ggpubr)
+ library(rstatix)
+ library(arsenal)
+ library(Biobase)
+ library(msigdbr)
+ library(kableExtra)
+ library(knitr)
+ library(REDCapR)
+ library(data.table)
+ library(emmeans)
+ library(NMF)
+ library(pheatmap)
+ library(UpSetR)
+ library(enrichR)
+ library(WriteXLS)
+ library(SAVER)
+ library(readxl)
+ library(limma)
+ library(edgeR)
+ library(BiocGenerics)
+ library(GSEABase)
+ library(slingshot)
+ library(SingleCellExperiment)
+ library(MAST)
+ library(muscat)
+ library(scater)
+ library(Seurat)
+ library(jsonlite)
+ library(dplyr)
+ library(glmmTMB)
+ library(reshape2)
+ library(broom.mixed)
+ library(nebula)
+ #library(table1)
+ library(clusterProfiler)
+ library('org.Hs.eg.db')
+ 
+ 
+ 
+ 
 
 
 
-
+ 
+ 
+ 
+ harmonized_data <- read.csv("C:/Users/netio/OneDrive - UW/Laura Pyle's files - Biostatistics Core Shared Drive/Data Harmonization/Data Clean/harmonized_dataset.csv", na = '')
+ 
+ dat <- harmonized_data %>% 
+   dplyr::select(-dob) %>% 
+   arrange(date_of_screen) %>% 
+   dplyr::summarise(
+     across(where(negate(is.numeric)), ~ ifelse(all(is.na(.x)), NA_character_, last(na.omit(.x)))),
+     across(where(is.numeric), ~ ifelse(all(is.na(.x)), NA_real_, mean(na.omit(.x), na.rm=T))),
+     .by = c(record_id, visit)
+   )
+ 
+ dat <- dat %>% filter(group == 'Lean Control')
+ 
+ 
+ data_dictionary <- readxl::read_xlsx('/Users/netio/Downloads/data_dictionary_master.xlsx')
+ 
+ data_dictionary_small <- data_dictionary %>% 
+   filter()
+ 
+ 
 
 
 
