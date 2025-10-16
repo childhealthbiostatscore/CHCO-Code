2e032d964dcffc02ff8c7e2fdf7e3f22646d6752
Dylan <135753623+netiony@users.noreply.github.com>
2025-10-16 14:59:24 -0700

    Create Brain_T1_Analysis_Plotting.R

---
+++ Petter Bjornstad/Brain Biomarkers/Dylan_Analyses/Brain_T1_Analysis_Plotting.R
@@ -0,0 +1,134 @@
+############### Brain Data Analysis
+
+library(purrr)
+library(dplyr)
+library(stringr)
+library(tidyverse)
+library(ggplot2)
+
+
+
+harmonized_data <- read.csv("C:/Users/netio/OneDrive - UW/Laura Pyle's files - Biostatistics Core Shared Drive/Data Harmonization/Data Clean/harmonized_dataset.csv", na = '')
+
+#date_of_screen
+#screen_date
+
+#dat <- harmonized_data %>% dplyr::select(-dob) %>% 
+#  arrange(date_of_screen) %>% 
+#  dplyr::summarise(across(where(negate(is.numeric)), ~ ifelse(all(is.na(.x)), NA_character_, first(na.omit(.x)))),
+#                   across(where(is.numeric), ~ ifelse(all(is.na(.x)), NA_real_, first(na.omit(.x)))),
+#                   .by = c(record_id, visit))
+
+
+
+dat <- harmonized_data %>% dplyr::select(-dob) %>% 
+  arrange(date_of_screen) %>% 
+  dplyr::summarise(across(where(negate(is.numeric)), ~ ifelse(all(is.na(.x)), NA_character_, last(na.omit(.x)))),
+                   across(where(is.numeric), ~ ifelse(all(is.na(.x)), NA_real_, mean(na.omit(.x), na.rm=T))),
+                   .by = c(record_id, visit))
+
+
+
+mri_ids <- c('CRC-10', 'CRC-11', 'CRC-12', 'CRC-13', 'CRC-26', 'CRC-39', 'CRC-46', 'CRC-51', 'CRC-53', 
+             'CRC-55', 'CRC-58', 'CRC-60', 
+             'RH2-01-O', 'RH2-03-O', 'RH2-08-T', 'RH2-10-L', 'RH2-11-O', 'RH2-13-O', 'RH2-16-O', 'RH2-17-L', 
+             'RH2-18-O', 'RH2-19-T', 'RH2-22-T', 'RH2-24-L', 'RH2-27-L', 'RH2-28-L', 'RH2-29-L', 'RH2-33-L', 
+             'RH2-34-O', 'RH2-35-T', 'RH2-38-T', 'RH2-39-O', 'RH2-41-T', 'RH2-42-T', 'RH2-43-T', 
+             'RH2-44-T', 'RH2-45-T', 'RH2-48-T', 'RH2-49-T', 'RH2-50-L', 'RH2-52-T', 'RH2-53-T', 
+             'RH2-55-T')
+
+mri_ids_df <- data.frame(ID = mri_ids, file_id = c(
+  'crc_10', 'crc_11', 'crc_12', 'crc_13', 'crc_26', 'crc_39', 'crc_46', 'crc_51', 'crc_53', 
+  'crc_55', 'crc_58', 'crc_60', 
+  'RH2_01_O', 'RH2_03_O', 'RH2_08_T', 'RH2_10_L', 'RH2_11_O', 'RH2_13_O', 'RH2_16_O', 'RH2_17_L', 
+  'RH2_18_O', 'RH2_19_T', 'RH2_22_T', 'RH2_24_L', 'RH2_27_L', 'RH2_28_L', 'RH2_29_L', 'RH2_33_L', 
+  'RH2_34_O', 'RH2_35_T', 'RH2_38_T', 'RH2_39_O', 'RH2_41_T', 'RH2_42_T', 'RH2_43_T', 
+  'RH2_44_T', 'RH2_45_T', 'RH2_48_T', 'RH2_49_T', 'RH2_50_L', 'RH2_52_T', 'RH2_53_T', 
+  'RH2_55_T'
+))
+
+
+small_dat <- dat %>% 
+  filter(record_id %in% mri_ids)
+
+#find missing
+#mri_ids[which(!mri_ids %in% small_dat$record_id)]
+
+#missing_dat <- dat %>% filter(rh2_id == 'RH2-38-O')
+
+
+small_dat$group[which(small_dat$record_id == 'RH2-38-T')] <- 'Obese Control'
+small_dat$record_id[which(small_dat$record_id == 'RH2-38-T')] <- 'RH2-38-O'
+
+
+
+
+
+qx_var <- c("ab40_avg_conc","ab42_avg_conc","tau_avg_conc",
+            "nfl_avg_conc","gfap_avg_conc","ptau_181_avg_conc","ptau_217_avg_conc")
+
+
+
+
+
+small_dat <- small_dat %>% 
+  dplyr::select(record_id, group, ab40_avg_conc, ab42_avg_conc, tau_avg_conc, 
+                nfl_avg_conc, gfap_avg_conc, ptau_181_avg_conc, ptau_217_avg_conc)
+
+
+
+t2d_ids <- small_dat$record_id[which(small_dat$group == 'Type 2 Diabetes')]
+lc_ids <- small_dat$record_id[which(small_dat$group == 'Lean Control')]
+
+
+
+small_dat <- small_dat %>% left_join(mri_ids_df, by = c('record_id'='ID'))
+
+
+
+
+
+#Data 
+subcortical <- data.table::fread("/Users/netio/Documents/UofW/Projects/Brain_fMRI_Analysis/t1_MRI/subcortical_volumes.txt")
+
+l_thick <- data.table::fread("/Users/netio/Documents/UofW/Projects/Brain_fMRI_Analysis/t1_MRI/lh_thickness.txt") 
+r_thick <- data.table::fread("/Users/netio/Documents/UofW/Projects/Brain_fMRI_Analysis/t1_MRI/rh_thickness.txt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
