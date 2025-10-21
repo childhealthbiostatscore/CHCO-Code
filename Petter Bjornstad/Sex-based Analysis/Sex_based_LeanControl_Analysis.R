40b41da2f635b87a5f0a26b95c818881f438b4f3
Dylan <135753623+netiony@users.noreply.github.com>
2025-10-21 08:36:41 -0700

    Update Sex_based_LeanControl_Analysis.R

--- Petter Bjornstad/Sex-based Analysis/Sex_based_LeanControl_Analysis.R
+++ Petter Bjornstad/Sex-based Analysis/Sex_based_LeanControl_Analysis.R
@@ -2578,3 +2578,118 @@ folder_path <- "/Users/netio/Documents/UofW/Projects/Sex_based_Analysis/LeanCont
  
  
  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ ########## Lineage Tracing (Slingshot) Analysis in Lean Controls (PT Cells)
+ 
+ library(slingshot)
+ library(condiments)
+ 
+ 
+ library(scran)
+ library(future)
+ library(future.apply)
+ library(tidyverse)
+ library(colorspace)
+ library(patchwork)
+ library(ggdendro)
+ library(cowplot)
+ library(ggpubr)
+ library(rstatix)
+ library(arsenal)
+ library(Biobase)
+ library(msigdbr)
+ library(kableExtra)
+ library(knitr)
+ library(REDCapR)
+ library(data.table)
+ library(emmeans)
+ library(NMF)
+ library(pheatmap)
+ library(UpSetR)
+ library(enrichR)
+ library(WriteXLS)
+ library(SAVER)
+ library(readxl)
+ library(limma)
+ library(edgeR)
+ library(BiocGenerics)
+ library(GSEABase)
+ library(slingshot)
+ library(SingleCellExperiment)
+ library(MAST)
+ library(muscat)
+ library(scater)
+ library(Seurat)
+ library(jsonlite)
+ library(dplyr)
+ library(glmmTMB)
+ library(reshape2)
+ library(broom.mixed)
+ library(nebula)
+ #library(table1)
+ library(clusterProfiler)
+ library('org.Hs.eg.db')
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ load('C:/Users/netio/Documents/UofW/Rockies/Hailey_Dotplots/No_Med_line700.Rdata')
+ 
+ so_subset <- so_kpmp_sc
+ remove(so_kpmp_sc)
+ 
+ #dat_groups <- data.table::fread('C:/Users/netio/Documents/UofW/Rockies/ROCKIES_GroupAssignments.txt')
+ #dat_groups <- dat_groups %>% filter(group2 %in% c('Lean Control', 'T2D-No SGLTi2'))
+ 
+ #so_subset <- subset(so_subset, record_id == dat_groups$record_id)
+ test <- so_subset@meta.data %>% dplyr::select(record_id, group) %>% filter(!duplicated(record_id))
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ dir.results <- 'C:/Users/netio/Documents/UofW/Projects/Sex_based_Analysis/LeanControl_Only/'
+ 
+ 
+ so_subset <- subset(so_subset, subset = record_id != 'CRC-55')
+ so_subset <- subset(so_subset, subset = group == 'Lean_Control')
+ test <- so_subset@meta.data %>% dplyr::select(record_id, group) %>% filter(!duplicated(record_id))
+ 
+ 
+ 
+ #PT Cells
+ so_subset <- subset(so_subset, subset = celltype2 == 'PT')
+ so_subset <- RunUMAP(so_subset, dims = 1:30)
+ 
+ sling_res <- slingshot(as.SingleCellExperiment(so_subset), clusterLabels = 'KPMP_celltype', 
+                        start.clus = 'PT-S1', end.clus = 'aPT', reducedDim = 'UMAP')
+ 
+ so_subset$pseudotime <- slingPseudotime(sling_res)[,1]
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
