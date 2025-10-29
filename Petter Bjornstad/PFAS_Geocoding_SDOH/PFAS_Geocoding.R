aa367df9878b0c5cd1ede4ee64ac725c07bb9b98
Dylan <135753623+netiony@users.noreply.github.com>
2025-10-28 20:57:21 -0700

    Update PFAS_Geocoding.R

--- Petter Bjornstad/PFAS_Geocoding_SDOH/PFAS_Geocoding.R
+++ Petter Bjornstad/PFAS_Geocoding_SDOH/PFAS_Geocoding.R
@@ -275,6 +275,214 @@ contaminant_wide_small <- contaminant_wide %>% filter(ZIPCODE %in% full_zip$zip_
 
 
 
+########## State-level data 
+
+states <- readxl::read_xlsx("/Users/netio/Downloads/PFAS2020SamplingProject_DrinkingWaterResults.xlsx")
+
+library(zipcodeR)
+get_tracts('80301')
+all_co_zips <- search_state('CO')
+
+
+rh <- readxl::read_excel(fs::path(dir_home, 
+                                  "drinking_water_pfas", 
+                                  "rh_zips.xlsx")) |>
+  mutate(zipcode = as.character(zipcode)) |> 
+  group_by(zipcode) |> summarize(nparticipants = length(zipcode)) |> ungroup()
+
+# Get city name for each zip code
+rh_zips <- tidylog::right_join(rh, all_co_zips)
+
+
+
+
+# Match my zips with list of CO zips
+co_zips2 <- co_zips |> 
+  tidylog::left_join(rh_zips,
+                     by = c("ZCTA5CE10" = "zipcode")) |> 
+  mutate(is_in_list = ifelse(!is.na(nparticipants), 
+                             "RH participant",
+                             "Other Zip Codes"), 
+         nparticipants_complete = if_else(is.na(nparticipants), 
+                                          0, nparticipants), 
+         major_city = toupper(major_city))
+
+
+# Front Range Counties
+frontrange_counties <-  c("Denver County",  
+                          "Arapahoe County", 
+                          "Jefferson County", 
+                          "Adams County", 
+                          "Douglas County", 
+                          "Broomfield County", 
+                          "Elbert County", 
+                          # "Park County", 
+                          "Gilpin County",
+                          "Clear Creek County", 
+                          # Colorado Springs Metropolitan Area
+                          "El Paso County", 
+                          "Teller County", 
+                          # Other
+                          "Boulder County",
+                          # "Eagle County",
+                          "Morgan County",
+                          "Otero County",
+                          "Pueblo County",
+                          "Washington County",
+                          "Weld County")
+
+
+
+
+participant_counties <- co_zips2 |>
+  filter(!is.na(nparticipants))
+
+# Filter only front range counties 
+denver_metropolitin_area <- co_zips2 |> 
+  filter(county %in% frontrange_counties, 
+         zipcode_type != "PO Box") 
+
+# Plot RH Participant Locations
+ggplot(data = denver_metropolitin_area |> filter(county == "El Paso County")) + 
+  geom_sf(aes(fill = nparticipants_complete), linewidth = .001) + 
+  theme_minimal() +
+  labs(title = "Participant Locations",
+       fill = "Number of Participants")
+
+# Renal-Heir Home prices vs. Colorado Home prices
+ggplot(denver_metropolitin_area |> 
+         filter(!is.na(median_home_value), 
+                zipcode_type != "PO Box"), 
+       aes(x = is_in_list, y = median_home_value)) + 
+  geom_boxplot()
+
+# Read in Colorado drinking water PFAS results --------------------
+co_pfas <- readxl::read_excel(fs::path(dir_home, 
+                                       "drinking_water_pfas", 
+                                       "PFAS2020SamplingProject_DrinkingWaterResults.xlsx"),
+                              col_names = TRUE) |>
+  janitor::clean_names() |>
+  janitor::remove_constant() |>
+  mutate(city_town = if_else(str_detect(loc_desc, " CITY OF") | 
+                               str_detect(loc_desc, " TOWN OF"), 
+                             str_remove(loc_desc, " CITY OF") |> 
+                               str_remove(" TOWN OF"), 
+                             "not city water"))
+
+
+# Filter Detected water systems
+co_pfas_ep <- co_pfas |> 
+  tidylog::filter(#detect_flag == "Y", 
+    loc_type_2 == "ENTRY POINT",  chemical_name == "Total PFAS" 
+  ) |> 
+  mutate(result_numeric2 = if_else(is.na(result_numeric), 0, result_numeric))
+
+
+# Summarize by loc_desc 
+co_pfas_summary <- co_pfas_ep |> 
+  group_by(loc_desc, chemical_name) |> 
+  summarise(mean_pfas = mean(result_numeric, na.rm = TRUE) |>
+              replace_na(0), 
+            n_samples = length(loc_desc),  
+            city_town = city_town[1]) |>
+  ungroup()
+
+# # Filter not city water
+# not_city_water <- co_pfas_ep |> 
+#   tidylog::filter(city_town == "not city water")
+
+
+# Merge participant locations with water districts
+participant_pfas <- tidylog::left_join(participant_counties, co_pfas_summary, 
+                                       by = c("major_city" = "city_town"))
+
+
+
+# Make plot of PK estimated PFAS
+# using steady-state ratios implied by the serum PFAS calculator PK models (Lu and Bartell, 2020) 
+pfas = c("PFOA", "PFOS", "PFHxS", "PFNA")
+S = c(118.35, 129.31, 201.57, 200.67)   
+B = c(1.67, 5.20, 1.30, 0.60)   # in ng/mL
+W = seq(0,40,10)   # in ng/L
+
+
+Cinf = S %o% W / 1000 + B           # from Bartell 2017
+
+# Convert to data frame and bind the W vector
+df <- as.data.frame(Cinf)
+rownames(df) <- pfas
+colnames(df) <- W
+
+df <- df |> as_tibble(rownames = "pfas")
+
+# Reshape from wide to long format
+df_long <- df %>%
+  pivot_longer(cols = -"pfas", 
+               names_to = "W", 
+               values_to = "concentration") |>
+  mutate(W = as.numeric(W))
+
+# 1a: pk modeled PFAS
+(p1 <- ggplot(df_long, aes(x=W, y=concentration, color=pfas, group = pfas)) +
+    geom_line(linewidth = 1.2) +
+    geom_vline(xintercept = 4, linetype = 2, color = "red", linewidth = 1.2) +
+    scale_color_brewer(type = "qual", palette = 6) +
+    labs(x = "Water PFAS concentration (ng/L)", 
+         y = "Expected serum\nPFAS (ng/mL)") +
+    xlim(c(0, 40)) +
+    theme(legend.position=c(.65, .25), 
+          legend.title = element_blank(),
+          axis.text.x = element_blank(), 
+          axis.title.x = element_blank()))
+
+# 1b: drinking water PFAS
+(p2 <- ggplot(participant_pfas |> filter(!is.na(mean_pfas)), 
+              aes(x=mean_pfas)) +
+    geom_vline(xintercept = 4, linetype = 2, color = "red", linewidth = 1.2) +
+    geom_histogram(aes(y = after_stat(count / sum(count))),
+                   binwidth = 10, 
+                   color = "black", 
+                   fill = "grey80", 
+                   breaks=seq(0,40,10)) + 
+    stat_bin(breaks=seq(0,40,10),
+             binwidth = 10, geom = "text", color = "black",
+             aes(y = after_stat(count / sum(count)), 
+                 label = scales::percent(after_stat(count / sum(count)))),
+             position = position_stack(vjust = 0.5)) + 
+    xlim(c(0, 40)) +
+    scale_y_continuous(labels = scales::percent) + 
+    labs(x = "Water PFAS levels (ng/L)", 
+         y = "Renal-HEIR\nParticipants (%)"))
+
+
+# Combine plots 
+combined_plot <- cowplot::plot_grid(NULL, p1, 
+                                    NULL, p2, 
+                                    ncol = 1, 
+                                    align = "v", 
+                                    rel_heights = c(.05, .5, .05, .5))
+
+ggsave(filename = fs::path(dir_figure, "Figure PK modeled PFAS.jpeg"), 
+       height = 5, width = 4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
