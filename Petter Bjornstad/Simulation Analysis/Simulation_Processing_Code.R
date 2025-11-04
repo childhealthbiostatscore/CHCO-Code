1f685a813684de75b4c833beec68540f9a9c222c
Hailey <hhampson@usc.edu>
2025-11-04 11:17:07 -0800

    new file paths

--- Petter Bjornstad/Simulation Analysis/Simulation_Processing_Code.R
+++ Petter Bjornstad/Simulation Analysis/Simulation_Processing_Code.R
@@ -2,4 +2,13 @@
 #Load Libraries
 
 #Set up Directories
-dir.dat <- c("/Users/hhampson/Library/CloudStorage/OneDrive-UW/Biostatistics Core Shared Drive/Hailey Hampson/1_Ongoing Projects/Microbiome Manuscript/Simulation Results")
+dir.dat <- c("/Users/hhampson/Library/CloudStorage/OneDrive-UW/Biostatistics Core Shared Drive/Hailey Hampson/1_Ongoing Projects/Microbiome Manuscript/Simulation Study/Simulation Data")
+dir.results <- c("/Users/hhampson/Library/CloudStorage/OneDrive-UW/Biostatistics Core Shared Drive/Hailey Hampson/1_Ongoing Projects/Microbiome Manuscript/Simulation Study/Simulation Results")
+
+#Load Data
+sim1a <- readRDS(fs::path(dir.dat,"all_iters_combined_scn1_1to500.RDS"))
+sim1b <- readRDS(fs::path(dir.dat,"all_iters_combined_scn1_501to1000.RDS"))
+sim1 <- rbind(sim1a,sim1b)
+rm(sim1a,sim1b)
+
+#
