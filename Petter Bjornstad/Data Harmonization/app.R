2d2a4cd894702871fbcfe22e6bae376df92d654e
laurapyle <laurapyle@gmail.com>
2025-12-02 18:37:43 -0800

    path

--- Petter Bjornstad/Data Harmonization/app.R
+++ Petter Bjornstad/Data Harmonization/app.R
@@ -31,6 +31,9 @@ if (user == "choiyej") {
 } else if (user == "pylell") {
   base_data_path <- "/Users/pylell/Library/CloudStorage/OneDrive-UW/Bjornstad/Biostatistics Core Shared Drive/"
   git_path <- "/Users/pylell/Documents/GitHub/CHCO-Code/Petter Bjornstad/"
+} else if (user == "lpyle") {
+  base_data_path <- "/Users/lpyle/Library/CloudStorage/OneDrive-UW/Bjornstad/Biostatistics Core Shared Drive/"
+  git_path <- "/Users/lpyle/Documents/GitHub/CHCO-Code/Petter Bjornstad/"
 } else if (user == "shivaniramesh") {
   base_data_path <- "/Users/shivaniramesh/Library/CloudStorage/OneDrive-UW/Laura Pyle's files - Biostatistics Core Shared Drive/"
   git_path <- "/Users/pylell/Documents/GitHub/CHCO-Code/Petter Bjornstad/"
