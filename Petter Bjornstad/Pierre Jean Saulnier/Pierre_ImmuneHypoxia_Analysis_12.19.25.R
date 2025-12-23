5b9cb6659a448428a10d5e8b7891a403a995f1ce
Dylan <135753623+netiony@users.noreply.github.com>
2025-12-23 10:45:50 -0800

    Create Pierre_ImmuneHypoxia_Analysis_12.19.25.R

---
+++ Petter Bjornstad/Pierre Jean Saulnier/Pierre_ImmuneHypoxia_Analysis_12.19.25.R
@@ -0,0 +1,31 @@
+####Monocytes Analysis: Hypoxia 
+
+immune_genes <- c(
+  'CD3D', 'CD3E', 'CD3G', 'CD4', 'CD8A', 'IL7R', 'TBX21', 'GZMK', 'IFNG', 'FOXP3', #Lymphocytest T
+  'CD19', 'MS44A1', 'CD20', 'CD79A', 'CD79B', #Lymphocytes B
+  'CD14', 'CD16', 'FCGR3A', 'CCR2', 'ENTPD1', 'CD39', 'S100A8', 'S100A9', 'LYZ', 'CD68' #monocytes
+  'CD68', 'MRC1', 'CD206', 'MARCO', 'TREM2', 'TREM1', 'CX3CR1', 'SELENOP', 'TNF', 'HLA-DRA', 'HLA-DRB1', #macrophages
+  'CD1C', 'ITGAX', 'CLEC9A', 'CD11C', #dendritic cells
+  'RIG-I', 'MDA5','TLR3', 'cGAS', 'STING', 'MAVS', 'TRIF', 'MyD88', 'OAS1', 'RSAD2', 'IFIT2', 'MX1', 'MX2', 'CXCL10', #innate
+  'IFN', 'IFN1', 'IL-28A',' IFNL2', 'IL29', 'IFNAR', 'STAT1', 'STAT2', 'JAK', 'IRF3', 'IRF7', 'IRF9', 'NFKB', 'TGFBR1', 'TGFBR2', 'ACVR1B', 'IL17A', 'RORC'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
