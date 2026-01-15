47150a7456414f8a82bcfba1f63aede8e5fbd2c3
choiyej12 <yeji.choi@cuanschutz.edu>
2026-01-14 17:33:07 -0800

    attempt figures for STM and figures for Anil's presentation

--- Petter Bjornstad/Renal HEIRitage/05_scRNA_results_processing_visualization.R
+++ Petter Bjornstad/Renal HEIRitage/05_scRNA_results_processing_visualization.R
@@ -7,7 +7,7 @@ library(enrichR)
 user <- Sys.info()[["user"]]
 
 if (user == "choiyej") { # local version
-  root_path <- "/Users/choiyej/Library/CloudStorage/OneDrive-SharedLibraries-UW/Laura Pyle - Bjornstad/Biostatistics Core Shared Drive"
+  root_path <- "/Users/choiyej/Library/CloudStorage/OneDrive-UW/Bjornstad/Biostatistics Core Shared Drive"
   git_path <- "/Users/choiyej/GitHub/CHCO-Code/Petter Bjornstad"
   keys <- fromJSON("/Users/choiyej/Library/CloudStorage/OneDrive-TheUniversityofColoradoDenver/Bjornstad Pyle Lab/keys.json")
 } else if (user == "rameshsh") { # hyak version
