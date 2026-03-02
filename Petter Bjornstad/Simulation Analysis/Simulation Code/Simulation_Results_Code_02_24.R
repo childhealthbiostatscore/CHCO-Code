9ffcdbffe0de93955baa2ab42d8a07aacafde7f2
Hailey <hhampson@usc.edu>
2026-03-02 08:39:45 -0800

    update selection

--- Petter Bjornstad/Simulation Analysis/Simulation Code/Simulation_Results_Code_02_24.R
+++ Petter Bjornstad/Simulation Analysis/Simulation Code/Simulation_Results_Code_02_24.R
@@ -78,11 +78,12 @@ simulation.parameters <- readRDS(fs::path(dir.dat,"Simulation_Scenarios_03_12_25
 sim.par.all <- read.csv(fs::path(dir.dat,"Simulation_Parameters.csv"))
 sim.par.all <- sim.par.all %>% 
   dplyr::select(-X) %>% 
-  # dplyr::select(c("P.e.causal","P.s.causal.all","N","OR.exposure")) %>% 
+  dplyr::select(c("P.e.causal","P.s.causal.all","N","OR.exposure")) %>%
   # dplyr::rename(P.s.scenario=P.s.causal.all)
 
 #4. Format Results ----
 results.data <- results
+
 format.fxn <- function(results.data){
   results.data <- results.data %>%
     dplyr::mutate(component=case_when(component=="Count model coefficients" ~ "Means",
@@ -280,6 +281,7 @@ format.fxn <- function(results.data){
 dir.sim.data <- c("/Users/hhampson/Library/CloudStorage/OneDrive-UW/Biostatistics Core Shared Drive/Hailey Hampson/1_Ongoing Projects/Microbiome Manuscript/Simulation Data 01_08/Simularion Results Data/Final")
 all_formatted_results <- data.frame()
 # temp_file <- tempfile(fileext = ".RDS")
+scenario <- 6
 for (scenario in 1:9) {
   # for (scenario in c(1,2,4:9)) {
   # s3$download_file(bucket,paste0("scn",scenario,"_iters1_to_1000.RDS"), temp_file)
@@ -289,6 +291,7 @@ for (scenario in 1:9) {
   all_formatted_results <- rbind(all_formatted_results,formatted.results)
 }
 rm(formatted.results)
+
 
 #Save formatted results
 # saveRDS(all_formatted_results,fs::path(dir.dat,"Formatted_Simulation_Results.rds"))
