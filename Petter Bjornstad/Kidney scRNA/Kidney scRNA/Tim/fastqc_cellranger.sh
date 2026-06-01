345e955cef58364cb0e32b17271595565cb77c0a
Tim Vigers <tvigers@uw.edu>
2026-06-01 16:57:40 -0700

    organoid cell ranger code (doesn't work because the FASTQs aren't 10X)

--- Petter Bjornstad/Kidney scRNA/Kidney scRNA/Tim/fastqc_cellranger.sh
+++ Petter Bjornstad/Kidney scRNA/Kidney scRNA/Tim/fastqc_cellranger.sh
@@ -3,7 +3,6 @@
 # QC
 #-------------------------------------------------------------------------------
 # Define input and output directories
-alias s3togo='s3cmd -c ~/.s3cfg_togo'
 INPUT_DIR="/home/tvigers/Documents/Data/UWMDI/kidney_organoids/data_raw/organoids"
 OUTPUT_DIR="/home/tvigers/Documents/Data/UWMDI/kidney_organoids/data_clean/organoids"
 S3_BUCKET="s3://scrna/Kidney organoids"
@@ -12,23 +11,6 @@ s3togo() { s3cmd -c ~/.s3cfg_togo "$@"; }
 export -f s3togo
 mkdir -p "${INPUT_DIR}"
 s3togo sync "${S3_BUCKET}/" "${INPUT_DIR}/" -v --check-md5 --exclude="data_clean/*"
-# Create the output directory if it doesn't already exist
-mkdir -p "${OUTPUT_DIR}/qc"
-# Run FastQC
-fastqc -t 4 -o "${OUTPUT_DIR}/qc" "${INPUT_DIR}"/*.fastq-*.gz
-echo "FastQC analysis complete!"
-# Run MultiQC
-echo "Starting MultiQC..."
-multiqc "${OUTPUT_DIR}/qc" -o "${OUTPUT_DIR}/qc" --no-data-dir
-# Sync to S3
-echo "Syncing results to S3..."
-s3togo sync "${OUTPUT_DIR}/qc" "$S3_BUCKET/data_clean/qc/" -v --check-md5 --delete-removed
-echo "QC complete!"
-#-------------------------------------------------------------------------------
-# Cell Ranger
-#-------------------------------------------------------------------------------
-# Define directories and parameters
-TRANSCRIPTOME="/home/tvigers/Documents/References/refdata-gex-GRCh38-2024-A"
 # Annoyingly, these FASTQ files have a weird number suffix (e.g. .fastq-030.gz).
 # Copy and rename them into a temporary directory
 TEMP_FASTQ_DIR="/home/tvigers/Documents/Data/UWMDI/kidney_organoids/data_clean/temp_fastqs"
@@ -46,6 +28,23 @@ for filepath in "$INPUT_DIR"/*.fastq-*.gz; do
     # Copy the file to the temp directory with the new name
     cp "$filepath" "$TEMP_FASTQ_DIR/$new_name"
 done
+# Create the output directory if it doesn't already exist
+mkdir -p "${OUTPUT_DIR}/qc"
+# Run FastQC
+fastqc -t 4 -o "${OUTPUT_DIR}/qc" "${TEMP_FASTQ_DIR}"/*.fastq-*.gz
+echo "FastQC analysis complete!"
+# Run MultiQC
+echo "Starting MultiQC..."
+multiqc "${OUTPUT_DIR}/qc" -o "${OUTPUT_DIR}/qc" --no-data-dir
+# Sync to S3
+echo "Syncing results to S3..."
+s3togo sync "${OUTPUT_DIR}/qc" "$S3_BUCKET/data_clean/qc/" -v --check-md5 --delete-removed
+echo "QC complete!"
+#-------------------------------------------------------------------------------
+# Cell Ranger
+#-------------------------------------------------------------------------------
+# Define directories and parameters
+TRANSCRIPTOME="/home/tvigers/Documents/References/refdata-gex-GRCh38-2024-A"
 # Make output folder
 mkdir -p "${OUTPUT_DIR}/cellranger"
 cd "${OUTPUT_DIR}/cellranger" || exit
@@ -75,5 +74,3 @@ for SAMPLE in $SAMPLES; do
     echo "Finished processing $SAMPLE"
 done
 echo "All samples processed successfully!"
-# Delete temp files
-rm -rf "${TEMP_FASTQ_DIR}"
