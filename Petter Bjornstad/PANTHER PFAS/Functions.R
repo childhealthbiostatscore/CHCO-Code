72dfb574aeeee3cfb7a4fbaf7d847dea52d6f381
sleidholt <104378614+sleidholt@users.noreply.github.com>
2026-06-23 12:14:52 -0700

    Update Functions.R

--- Petter Bjornstad/PANTHER PFAS/Functions.R
+++ Petter Bjornstad/PANTHER PFAS/Functions.R
@@ -496,28 +496,17 @@ plot_model_2a <- function(data,outcomes = group_time_outcomes,save_pdf = FALSE,o
       theme_bw() +
       labs(
         title = paste("Unadjusted", get_outcome_label(outcome), "over time by group"),
-        subtitle = subtitle_text,
-        x = "Visit",
-        y = get_outcome_label(outcome),
-        color = "Group"
+        #subtitle = subtitle_text,
+        #x = "Visit",
+        #y = get_outcome_label(outcome),
+        #color = "Group"
       )
     
     plot_list[[i]] <- p
   }
   
   plot_list <- plot_list[!vapply(plot_list, is.null, logical(1))]
-  
-  if (save_pdf && length(plot_list) > 0) {
-    pdf(
-      file.path(out_dir, "group_time_all_outcomes.pdf"),
-      width = 8,
-      height = 6
-    )
-    for (nm in names(plot_list)) {
-      print(plot_list[[nm]])
-    }
-    dev.off()
-  }
+
   
   return(plot_list)
 }
